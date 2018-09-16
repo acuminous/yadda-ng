8d205116aa05b4fdf7bc28f938c2706875f2dab8
Stephen Cresswell <git@stephen-cresswell.net>
2018-09-16 18:24:40 +0100

    Improve example

--- examples/test/index.test.js
+++ examples/test/index.test.js
@@ -1,9 +1,10 @@
 const { Gherkish, Playbooks, Convenience } = require('../..');
+const { MochaPlaybook } = Playbooks;
 const { compileFeatures } = Convenience;
 const { bottlesLibrary, convertersLibrary } = require('./libraries');
 
 Gherkish.Languages.utils.setDefault('English');
 
 const features = compileFeatures({ libraries: [ bottlesLibrary, convertersLibrary ] });
-const playbook = new Playbooks.MochaPlaybook({ features });
+const playbook = new MochaPlaybook({ features });
 playbook.run();
