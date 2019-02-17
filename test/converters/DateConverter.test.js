dbfb0d3d55a215f95dd88dec497470f4721dd364
Stephen Cresswell <git@stephen-cresswell.net>
2019-02-17 23:16:18 +0100

    Fix date converter test in different tz

--- test/converters/DateConverter.test.js
+++ test/converters/DateConverter.test.js
@@ -5,7 +5,7 @@ const { Converters } = require('../..');
 describe('DateConverter', () => {
 
   it('should convert a string to a date', async () => {
-    const converted = await new Converters.DateConverter().convert({}, '1/1/18');
+    const converted = await new Converters.DateConverter().convert({}, '1/1/18 00:00:00.000Z');
     expect(converted.toISOString()).toBe('2018-01-01T00:00:00.000Z');
   });
 
